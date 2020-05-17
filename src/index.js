6f1c90680bfb13ec95e404eadc4cc73125410733
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-05-16 22:57:39 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './styles.scss';
+
+const Index = () => {
+    return <div>Hello MotherFuckin World!</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
